0d401e8a3de3e82f4b21bfa5e40966e88710b08a
THANUJA NIRMAL <nirmalthanuja7@gmail.com>
2026-06-05 01:32:55 +0530

    Initial commit of backend

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ const app = express();
 // â”€â”€ SECURITY â”€â”€
 app.use(helmet());
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
-  credentials: true,
+  origin: "*",
 }));
 
 // Rate limiting
